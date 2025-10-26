40572c0c53c5a477f22b26010dbeca5be37b4f35
iRonSam007 <abida.invest10@gmail.com>
2025-10-26 23:15:50 +0100

    feat(book/presentation): add Search result and fovorites TabRow/Surface into BookListScreen

--- composeApp/src/commonMain/kotlin/org/example/project/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/book/presentation/book_list/BookListScreen.kt
@@ -2,23 +2,40 @@ package org.example.project.book.presentation.book_list
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.example.project.book.domain.Book
 import org.example.project.book.presentation.book_list.components.BookSearchBar
 import org.example.project.core.presentation.DarkBlue
+import org.example.project.core.presentation.DesertWhite
+import org.example.project.core.presentation.SandYellow
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
+import tuto_composemp.composeapp.generated.resources.Res
+import tuto_composemp.composeapp.generated.resources.favorites
+import tuto_composemp.composeapp.generated.resources.search_results
 
 
 @Composable
@@ -72,6 +89,64 @@ fun BookListScreen(
                 .fillMaxWidth()
                 .padding(16.dp)
         )
+
+        // version 2 - add Surface and TabRow for BookList:
+        Surface(
+            modifier = Modifier.weight(1f).fillMaxWidth(),
+            color = DesertWhite,
+            shape = RoundedCornerShape(
+                topStart = 32.dp,
+                topEnd = 32.dp
+            )
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                TabRow(
+                    selectedTabIndex = state.selectedTabIndex,
+                    modifier = Modifier
+                        .padding(vertical = 12.dp)
+                        .widthIn(max = 700.dp)
+                        .fillMaxWidth(),
+                    containerColor = DesertWhite,
+                    indicator = { tabPositions ->
+                        TabRowDefaults.SecondaryIndicator( //Todo: overwrite this indicator to offset the tab - remove purple bar
+                            color = SandYellow,
+                            modifier = Modifier.tabIndicatorOffset(tabPositions[state.selectedTabIndex])
+                        )
+                    }
+                ){
+                    Tab(
+                        selected = state.selectedTabIndex ==0,
+                        onClick = {onIntent(BookListIntent.OnTabSelected(0))},
+                        modifier= Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 0.5f)
+                    ){
+                        Text(
+                            text = stringResource(Res.string.search_results),
+                            modifier = Modifier.padding(vertical = 12.dp)
+                        )
+                    }
+                    Tab(
+                        selected = state.selectedTabIndex ==1,
+                        onClick = {onIntent(BookListIntent.OnTabSelected(1))},
+                        modifier= Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 0.5f)
+                    ){
+                        Text(
+                            text = stringResource(Res.string.favorites),
+                            modifier = Modifier.padding(vertical = 12.dp)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+
+//                HorizontalPager(){}
+            }
+        }
+
     }
 
 

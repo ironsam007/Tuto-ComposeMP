9448b0481f8fe3b9c610f7ecbd044f0c57ae1079
iRonSam007 <abida.invest10@gmail.com>
2025-10-26 22:50:03 +0100

    chore(book/presentation): add preview for BookListScreen

--- composeApp/src/androidMain/kotlin/org/example/project/Previews.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/Previews.kt
@@ -9,6 +9,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import org.example.project.book.domain.Book
+import org.example.project.book.presentation.book_list.BookListScreen
+import org.example.project.book.presentation.book_list.BookListState
 import org.example.project.book.presentation.book_list.components.BookListItem
 import org.example.project.book.presentation.book_list.components.BookSearchBar
 
@@ -30,9 +33,35 @@ private fun BookSearchBarPreview() {
     }
 }
 
-@Preview
+//@Preview
 @Composable
 fun AppAndroidPreview() {
     App()
 }
 
+private val books = (1..100).map{
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Philipp Lackner"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.67854,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
+
+@Preview
+@Composable
+fun BookListPreview(){
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onIntent = {}
+    )
+}

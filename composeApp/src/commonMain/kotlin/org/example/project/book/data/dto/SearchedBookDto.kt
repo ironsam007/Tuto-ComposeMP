99c8c33ebc15d61084a65c3cf320ab9c423d95ce
iRonSam007 <abida.invest10@gmail.com>
2025-10-27 11:40:43 +0100

    feat(book/data): define data model dto according to api json response

--- composeApp/src/commonMain/kotlin/org/example/project/book/data/dto/SearchedBookDto.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/book/data/dto/SearchedBookDto.kt
@@ -1,4 +1,21 @@
 package org.example.project.book.data.dto
 
-class SearchedBookDto {
-}
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+//Parse json into data class with kotlin serialization: annotation
+@Serializable
+data class SearchedBookDto(
+    @SerialName("key") val id: String, //key = remote naming
+    @SerialName("title") val title: String,
+    @SerialName("language") val languages: List<String>? = null,
+    @SerialName("cover_i") val coverAlternativeKey: Int? = null,
+    @SerialName("author_key") val authorKeys: List<String>? = null,
+    @SerialName("author_name") val authorNames: List<String>? = null,
+    @SerialName("cover_edition_key") val coverKey: String? = null,
+    @SerialName("first_publish_year") val firstPublishYear: Int? = null,
+    @SerialName("ratings_average") val ratingsAverage: Double? = null,
+    @SerialName("ratings_count") val ratingsCount: Int? = null,
+    @SerialName("number_of_pages_median") val numPagesMedian: Int? = null,
+    @SerialName("edition_count") val numEditions: Int? = null,
+)

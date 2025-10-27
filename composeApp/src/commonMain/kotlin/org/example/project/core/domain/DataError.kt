7fe37a018d861e7ab62405dd28b6c95035f525b5
iRonSam007 <abida.invest10@gmail.com>
2025-10-27 12:09:14 +0100

    feat(core/domain): implement DatError to response with if api call Failed

--- composeApp/src/commonMain/kotlin/org/example/project/core/domain/DataError.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/core/domain/DataError.kt
@@ -1,1 +1,24 @@
 package org.example.project.core.domain
+
+
+
+interface Error
+
+
+sealed interface DataError : Error {
+    enum class Remote: DataError {
+        REQUEST_TIMEOUT,
+        TOO_MANY_REQUEST,
+        NO_INTERNET,
+        SERVER,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local: DataError {
+        DISK_FULL,
+        UNKNOWN
+    }
+}
+
+

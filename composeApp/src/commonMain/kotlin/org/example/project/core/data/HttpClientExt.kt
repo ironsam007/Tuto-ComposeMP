e1f72f8a12a6cf331f721f5f806eda93b709ec95
iRonSam007 <abida.invest10@gmail.com>
2025-10-28 08:53:53 +0100

    feat(core/data): add utility functions for http response handling
     - add responseToResult() to convert Ktor HttpResponse into typed Result with explicit HTTP error mapping
    
     - add safeCall() to ensure getting a valid http response

--- composeApp/src/commonMain/kotlin/org/example/project/core/data/HttpClientExt.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/core/data/HttpClientExt.kt
@@ -1,2 +1,52 @@
 package org.example.project.core.data
 
+import io.ktor.client.call.NoTransformationFoundException
+import io.ktor.client.call.body
+import io.ktor.client.network.sockets.SocketTimeoutException
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.ensureActive
+import org.example.project.core.domain.DataError
+import org.example.project.core.domain.Result
+import kotlin.coroutines.coroutineContext
+
+
+//Utility functions for our growing app that could need to deal with multiple API endpoints
+// from ktor: HttpResponse.status typed as HttpStatusCode{description: String, value: Int}
+
+suspend inline fun <reified T>responseToResult(
+    response: HttpResponse,
+): Result<T, DataError.Remote>{
+    return when(response.status.value){
+        in 200..299 -> { //HttpStatusCode starts with 2 -> OK
+            try{
+                Result.Success(response.body<T>())
+            }catch(e: NoTransformationFoundException){
+                Result.Error(DataError.Remote.SERIALIZATION)
+            }
+        }
+        408 -> Result.Error(DataError.Remote.REQUEST_TIMEOUT)
+        429 -> Result.Error(DataError.Remote.TOO_MANY_REQUEST)
+        in 500..599 -> Result.Error(DataError.Remote.SERVER)
+        else -> Result.Error(DataError.Remote.UNKNOWN)
+    }
+}
+
+
+//ensure getting a valid API/http response
+suspend inline fun <reified T>safeCall(
+    execute: ()-> HttpResponse
+) : Result<T, DataError.Remote>
+{
+    val response = try {
+        execute()
+    } catch(e: SocketTimeoutException ){
+        return Result.Error(DataError.Remote.REQUEST_TIMEOUT)
+    } catch(e: UnresolvedAddressException){
+        return Result.Error(DataError.Remote.NO_INTERNET)
+    }catch(e: Exception){
+        coroutineContext.ensureActive() // If cancellation exception thrown by suspend function is catched such as "execute()" -> break this cancellation as the parent coroutine may not notified
+        return Result.Error(DataError.Remote.UNKNOWN)
+    }
+    return responseToResult(response)
+}

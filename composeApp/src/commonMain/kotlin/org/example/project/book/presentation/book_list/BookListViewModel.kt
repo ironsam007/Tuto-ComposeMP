6e85c08393e197147107fcbe03d95ffcaa5e09dc
iRonSam007 <abida.invest10@gmail.com>
2025-11-02 21:57:12 +0100

    refactor(book/presentation): add onStart and stateIn to make BookListViewModel's state lifecycle-aware and to prevent bg collection when ui is not observing state.

--- composeApp/src/commonMain/kotlin/org/example/project/book/presentation/book_list/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/book/presentation/book_list/BookListViewModel.kt
@@ -2,15 +2,16 @@ package org.example.project.book.presentation.book_list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.example.project.book.domain.Book
@@ -26,13 +27,41 @@ class BookListViewModel(
     private val bookRepository: BookRepository
 ): ViewModel() {
 
+    private var searchJob: Job? = null // for concurrency control: cancel search if there is new one
+    private var cachedBooks = emptyList<Book>()
+
 
 
     private val _state = MutableStateFlow(BookListState()) //Internal stateFlow, can be modified on VM level
-    val state = _state.asStateFlow() //exposed StateFlow that reflect the internal StateFlow
 
-    private var searchJob: Job? = null // for concurrency control: cancel search if there is new one
-    private var cachedBooks = emptyList<Book>()
+    /*
+    With .asStateFlow() only expose as readOnly stateflow:
+        -> no lifeCycle awareness: if no one collects, _state still exists but u can't delay or controll emission:
+        -> can't intercept when collection start: ability to run logic when ui starts observing.
+    With .onStart{} u can run side effects when the flow starts being collected,
+        -> in our case, call observeSearchQuery ony when cache is empty
+            => Lazily initialize only when needed, not immediately or at construction.
+
+    With .stateIn(): convert Flow into Hot StateFlow with controller sharing rules.
+
+    */
+    val state = _state
+        .onStart { //when flow start being collected
+        if (cachedBooks.isEmpty()) { //even after the flow start being collected, we will lazily observe search query only when cachedBooks is empty
+            observeSearchQuery()
+        }
+        }
+        .stateIn( //converting this flow into hot stateFlow, with sharing rules:
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000L), //After the last collector stops wait 5s before stopping upstream(this prevent rapid start/stop when screen rotate)
+            initialValue = _state.value // initial value to start from
+            )
+    }
+
+
+
+
+
 
     /*
     onIntent(): should be called within UI to handle all defined user intents on BookListScreen,
@@ -60,7 +89,7 @@ class BookListViewModel(
     /*
      observeSearchQuery(): Listen to any state change in searchQuery and trigger research
     */
-    private fun observerSearchQuery(){
+    private fun observeSearchQuery(){
         state
             .map {it.searchQuery} //create new Flow<String> that emits the latest searchQuery every time BookListState changes / Still a transformer but here, for over time
             .distinctUntilChanged()

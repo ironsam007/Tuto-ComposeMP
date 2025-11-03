527d00e7c640cde415f18d005a6bfaa0a28ebe37
iRonSam007 <abida.invest10@gmail.com>
2025-11-03 23:16:36 +0100

    feat: add actual declaration for expect/ anddroidMain and nativeMain module files

--- composeApp/src/commonMain/kotlin/org/example/project/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/di/Modules.kt
@@ -13,11 +13,17 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 
-expect val platformModule: Module
 
-val sharedModule =module {
-    single{ HttpClientFactory.create(get()) }
-    singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>()
+//KMP provide this concept of expect & actual, for it to be instructed to expect certain Module to be platform dependant:
+expect val platformModule: Module //click on add missing Actual declaration.=>Create **Module files**
+
+val sharedModule = module {  //this Module is a container for shared dependencies
+    single { HttpClientFactory.create(get()) } //the engine here is platform dependent -> get() instruct koin to try to get a dependency..to be set later, cuze if not provided, koin will crash.
+    //now that i have an HttpClientInstance, i can use it in KtorRemoteBookDataSource instantiation block.
+    //NB: why koin is seen as a service locater compared to Dagger -> got a pool of objects, when need one it access it from there.
+    //instead of single{xxx(get(),get())} for multiple fields, we use singleOf(::xxx) that instruct koin to create instance with dependencies it has already available(all constructor args if available):
+    //.bind() to specify the type of the instance that will be provided by koin after instantiating this implementation???? to check
+    singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>() //bind: provide him KtorRBDS when someone search for RemoteBookDataSource
     singleOf(::DefaultBookRepository).bind<BookRepository>()
 
     viewModelOf(::BookListViewModel)
---
+++ composeApp/src/nativeMain/kotlin/org/example/project/di/Modules.native.kt
@@ -0,0 +1,10 @@
+package org.example.project.di
+
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.darwin.Darwin
+import org.koin.dsl.module
+
+actual val platformModule: org.koin.core.module.Module
+    get() = module{
+        single<HttpClientEngine>{ Darwin.create() }
+    }

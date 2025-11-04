cef8508ad406f3202b5f48ac24c91cae9283f6b1
iRonSam007 <abida.invest10@gmail.com>
2025-11-04 09:17:41 +0100

    feat(android): add BookApplication as entry point to call initKoin as the app start-up

---
+++ composeApp/src/androidMain/kotlin/org/example/project/BookApplication.kt
@@ -0,0 +1,32 @@
+package org.example.project
+
+import android.app.Application
+import org.example.project.di.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.KoinApplication
+
+//Entry point for Android to call initKoin when the app is launched
+class BookApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initKoin { //KoinApplication is the receiver of this,
+            androidContext(this@BookApplication)
+        }
+    }
+}
+
+/* NB:
+from initKoin() declaration:    "fun initKoin(config: KoinAppDeclaration? =null)"
+KoinAppDeclaration defined as   "typealias KoinAppDeclaration = KoinApplication.() -> Unit"
+    => which means its a lambda with receiver of type KoinApplication
+    => Th lambda that will be passed to initKoin has an implicit receiver of type KoinApplication
+    => So, this will refer to KoinApplication instance that is created by startKoin{} in initKoin()
+    => we can call androidContext() as it is an extension function on the receiver KoinApplication
+    => KoinApplicatin.androidContext() will take BookApplication instance as the android context to register inside koin
+        -> any future di will be delt on this instance context.
+
+
+
+Kotlin's trailing-lambda syntax: Write lambda outside the parentheses
+
+*/

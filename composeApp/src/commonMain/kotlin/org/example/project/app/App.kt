d8ce3c84cd55e33e8dcab9e80b5f269e4bd2a51f
iRonSam007 <abida.invest10@gmail.com>
2025-11-07 22:53:48 +0100

    feat(app): define navigation Graph in App()

--- composeApp/src/commonMain/kotlin/org/example/project/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/app/App.kt
@@ -1,10 +1,18 @@
 package org.example.project.app
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import io.ktor.client.engine.HttpClientEngine
 import org.example.project.book.data.network.KtorRemoteBookDataSource
 import org.example.project.book.data.repository.DefaultBookRepository
@@ -17,21 +25,34 @@ import org.koin.compose.viewmodel.koinViewModel
 @Composable
 @Preview
 fun App() {
-//    val viewModel = koinViewModel<BookListViewModel>()
-//    BookListScreenRoot(
-//        viewModel = viewModel,
-//        onBookClick = {}
-//    )
 
-    MaterialTheme{
+
+    MaterialTheme {
         val navController = rememberNavController()
         NavHost(
             navController = navController,
             startDestination = Route.BookGraph
-        ){
-
+        ) {   //Define Navigation graph
+            composable<Route.BookList> {
+                val viewModel = koinViewModel<BookListViewModel>()
+                BookListScreenRoot(
+                    viewModel = viewModel,
+                    onBookClick = { book -> //when book clicked -> navigate
+                        navController.navigate(
+                            Route.BookDetail(book.id)
+                        )
+                    }
+                )
+            }
+            composable<Route.BookDetail> { entry -> //args we navigated with
+                val args = entry.toRoute<Route.BookDetail>()
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Book is ${args.id}")
+                }
+            }
         }
-
     }
-
 }

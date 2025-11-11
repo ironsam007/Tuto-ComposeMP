5213bcbefc779b95816d64d06f21b490d92480fc
iRonSam007 <abida.invest10@gmail.com>
2025-11-11 23:55:46 +0100

    feat(app): integrate SlectedBookViewModel in App() with  Nav graph

--- composeApp/src/commonMain/kotlin/org/example/project/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/app/App.kt
@@ -6,16 +6,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import io.ktor.client.engine.HttpClientEngine
 import org.example.project.book.data.network.KtorRemoteBookDataSource
 import org.example.project.book.data.repository.DefaultBookRepository
+import org.example.project.book.presentation.SelectedBookViewModel
 import org.example.project.book.presentation.book_list.BookListScreenRoot
 import org.example.project.book.presentation.book_list.BookListViewModel
 import org.example.project.core.data.HttpClientFactory
@@ -25,34 +28,49 @@ import org.koin.compose.viewmodel.koinViewModel
 @Composable
 @Preview
 fun App() {
-
-
     MaterialTheme {
         val navController = rememberNavController()
         NavHost(
             navController = navController,
             startDestination = Route.BookGraph
         ) {   //Define Navigation graph
-            composable<Route.BookList> {
-                val viewModel = koinViewModel<BookListViewModel>()
-                BookListScreenRoot(
-                    viewModel = viewModel,
-                    onBookClick = { book -> //when book clicked -> navigate
-                        navController.navigate(
-                            Route.BookDetail(book.id)
-                        )
+
+            navigation<Route.BookGraph>(startDestination = Route.BookList) {
+
+                composable<Route.BookList> {
+                    val viewModel = koinViewModel<BookListViewModel>()
+                    val selectedBokViewModel =
+                        it.sharedKoinViewModel<SelectedBookViewModel>(navController) //it: navBackStack entry
+
+                    LaunchedEffect(true) {
+                        selectedBokViewModel.onSelectedBook(null)  //BookSelection at Launch is null
                     }
-                )
-            }
-            composable<Route.BookDetail> { entry -> //args we navigated with
-                val args = entry.toRoute<Route.BookDetail>()
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text("Book is ${args.id}")
+
+                    BookListScreenRoot(
+                        viewModel = viewModel,
+                        onBookClick = { book -> //when book clicked -> navigate
+                            selectedBokViewModel.onSelectedBook(book) //integrate BookSelection VM at click
+
+                            navController.navigate(
+                                Route.BookDetail(book.id)
+                            )
+                        }
+                    )
+                }
+                composable<Route.BookDetail> { entry -> //args we navigated with
+                    val args = entry.toRoute<Route.BookDetail>()
+                    val selectedBookViewModel =
+                        entry.sharedKoinViewModel<SelectedBookViewModel>(navController)
+                    val selectedBook by selectedBookViewModel.selectedBook.collectAsStateWithLifeCycle()
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Book is $selectedBook")
+                    }
                 }
             }
         }
     }
 }
+

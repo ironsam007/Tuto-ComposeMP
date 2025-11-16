c6ad20cbf7452941ab730ff71a0cb7e47aed704b
iRonSam007 <abida.invest10@gmail.com>
2025-11-16 23:15:33 +0100

    feat(data/database): add expect Room database constructor for KMP support

---
+++ composeApp/src/commonMain/kotlin/org/example/project/book/data/database/BookDatabaseConstructor.kt
@@ -0,0 +1,13 @@
+package org.example.project.book.data.database
+
+import androidx.room.RoomDatabaseConstructor
+
+/* This is needed in KMP project for Room to work: Call the correct platform code to instantiate the room database
+Room generates platform-specific DB implementations, but does not generate the platform-specific code to create the database,
+    so we define an expect constructor in shared code and implement it with actual declarations for each platform,
+    while suppressing missing-actual errors until Room code is generated.
+*/
+@Suppress("NO_ACTUAL_FOR_EXPECT") //prohibit compiler from complaining about "missing actual" at source level cuz room compiler will provide them in generated code (when declare expect, kt requires actual implementation for each target )
+expect object BookDatabaseConstructor: RoomDatabaseConstructor<FavoriteBookDatabase> { //instruct Kotlin that There WILL be an implementation of database construction, but it depends on the platform.
+    override fun initialize(): FavoriteBookDatabase // initialize out database
+}

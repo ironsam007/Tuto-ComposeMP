8cb8707991c229251e01348082363a457f6c593f
iRonSam007 <abida.invest10@gmail.com>
2025-11-17 00:47:54 +0100

    feat(domain): add FavoriteBook logic into domain and necessary mappers

--- composeApp/src/commonMain/kotlin/org/example/project/book/domain/BookRepository.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/book/domain/BookRepository.kt
@@ -1,6 +1,8 @@
 package org.example.project.book.domain
 
+import kotlinx.coroutines.flow.Flow
 import org.example.project.core.domain.DataError
+import org.example.project.core.domain.EmptyResult
 import org.example.project.core.domain.Result
 
 
@@ -8,7 +10,11 @@ import org.example.project.core.domain.Result
 
 interface BookRepository {
     suspend fun searchBooks(query: String): Result<List<Book>, DataError.Remote>
-
     suspend fun getBookDescription(bookId: String): Result<String?, DataError> //error can be remote and local
+
+    fun getFavoriteBooks(): Flow<List<Book>> //List<BookEntity> need mapper so dont use data/BookEntity
+    fun isBookFavorite(id: String): Flow<Boolean>
+    suspend fun markAsFavorite(book: Book): EmptyResult<DataError.Local> //typealias for Result<Unit, E>
+    suspend fun deleteFromFavorites(id: String)
 
 }

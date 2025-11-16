5e5ffe3f1b48c13a973c616cf02264d575941789
iRonSam007 <abida.invest10@gmail.com>
2025-11-16 22:13:51 +0100

    refactor(data): instruct FavoriteBookDatabase to use TypeConverters

--- composeApp/src/commonMain/kotlin/org/example/project/book/data/database/FavoriteBookDatabase.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/book/data/database/FavoriteBookDatabase.kt
@@ -2,11 +2,16 @@ package org.example.project.book.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 
 
 @Database(
     entities = [BookEntity::class], //our database contains this table with header = BookEntity
     version =  1
+)
+@TypeConverters(
+    StringListTypeConverter::class
 )
 abstract class FavoriteBookDatabase: RoomDatabase() {
     abstract val favoriteBookDao: FavoriteBookDao
